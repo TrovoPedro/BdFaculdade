741e6c7f1a59d7a1d15bc2f3440654688f931df2
Pedro Trovo <98984115+TrovoPedro@users.noreply.github.com>
2024-03-24 15:13:21 -0300

    Update and rename trovoAtleta.sql to Join01.sql

--- Join01.sql
+++ Join01.sql
@@ -52,12 +52,49 @@ select * from atleta join pais
     
     
 create table musica (
-	idMusica int 
-
-
+	idMusica int,
+	titulo varchar(40),
+    artista varchar(40),
+    genero varchar(40),
+	fkAlbum int not null,
+    constraint fkAlbumMusica foreign key (fkAlbum)
+		references album(idAlbum)
 );
+
+create table album (
+	idAlbum int primary key auto_increment,
+    nomeAlbum varchar(40),
+    tipo varchar(10),
+    dtLanc date
+);
+
+alter table album
+	add constraint chkTipo
+	check(tipo in('digital','fisico'));
+
+insert into album values 
+	(default, 'Astroworld', 'digital', '2018-10-10'),
+	(default, 'Astroworld', 'digital', '2018-10-10'),
+	(default, '20', 'fisico', '2015-05-11');
     
-
+select * from album;
     
+update album set nomeAlbum = 'Ten'
+	where idAlbum = 3;
+    
+insert into musica values 
+	(default, 'Stargazing', 'Travis Scott', 'trap' , 1),
+	(default, 'Skeletons', 'Travis Scott', 'trap', 1),
+	(default, 'Even Flow', 'Pearl Jam', 'Rock', 3),
+	(default, 'Black', 'Pearl Jam', 'Rock', 3);
+    
+select * from musica join album
+	on idAlbum = fkAlbum;
 
-
+select titulo, nomeAlbum from musica join album
+	on idAlbum = fkAlbum;
+    
+select *, nomeAlbum from musica join album
+	on idAlbum = fkAlbum
+	where tipo = 'fisico';
+    
